8adf2daed52efb750d771546005b6cececa31c9e
Marco Peralta <marko.jp.17@gmail.com>
2026-04-20 20:54:05 +0100

    Add basic routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Landing } from './features/landing/landing';
+import { Library } from './features/library/library';
+import { GamePicker } from './features/game-picker/game-picker';
+import { GameDetail } from './features/game-detail/game-detail';
+import { AddGame } from './features/add-game/add-game';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Landing },
+  { path: 'library', component: Library },
+  { path: 'game-picker', component: GamePicker },
+  { path: 'game-detail', component: GameDetail },
+  { path: 'add-game', component: AddGame },
+];
---
+++ src/app/features/game-picker/game-picker.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game-picker',
+  imports: [],
+  templateUrl: './game-picker.html',
+  styleUrl: './game-picker.scss',
+})
+export class GamePicker {
+
+}
