24e37722dcf9b564d25f19a8a0405c1d232a8c1f
Marco Peralta <marko.jp.17@gmail.com>
2026-04-25 19:44:42 +0100

    Add landing page assets and enhance layout
    - Introduced new images for the landing page, including header and feature icons.
    - Updated landing page HTML structure to include detailed descriptions of features.
    - Enhanced styles for the landing page, improving layout and responsiveness.
    - Added new SCSS variables for better theme management and button styling.

--- src/app/features/landing/landing.ts
+++ src/app/features/landing/landing.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../core/services/auth.service';
+import { AuthModal } from '../../shared/components/auth-modal/auth-modal';
+import { Dialog, DialogModule } from '@angular/cdk/dialog';
 
 @Component({
   selector: 'app-landing',
-  imports: [],
+  imports: [DialogModule],
   templateUrl: './landing.html',
   styleUrl: './landing.scss',
 })
 export class Landing {
+  public authService = inject(AuthService);
+  private dialog = inject(Dialog);
 
+  openAuth(mode: 'login' | 'signup') {
+    const dialogRef = this.dialog.open(AuthModal, {
+      minWidth: '320px',
+      maxWidth: '450px',
+      data: { mode },
+    });
+  }
 }
